44c49d7ffca80710209ce5fb9d1c054d845312b8
김세준 <dailyzett@gmail.com>
2024-11-23 22:12:23 +0900

    최초 커밋

---
+++ keypad-server/src/test/kotlin/org/example/keypadserver/KeypadServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.keypadserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeypadServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
